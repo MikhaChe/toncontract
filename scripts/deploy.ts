d127b87c07132b5a5602e1af7c22da9d50e0ea2c
MikhaChe <feeltimenow@gmail.com>
2024-09-16 16:27:40 +0200

    начало тестирования

---
+++ scripts/deploy.ts
@@ -0,0 +1,25 @@
+import {hex} from "../build/main.compiled.json";
+import {beginCell, Cell, contractAddress, StateInit, storeStateInit} from "@ton/ton";
+
+async function deployScript() {
+
+  const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
+  const dataCell = new Cell();
+
+  const stateInit: StateInit = {
+    code: codeCell,
+    data: dataCell,
+  };
+
+  const stateInitBuilder = beginCell();
+  storeStateInit(stateInit)(stateInitBuilder);
+  const stateInitCell = stateInitBuilder.endCell();
+
+  const address = contractAddress(0, {
+    code: codeCell,
+    data: dataCell,
+  });
+  
+}
+
+deployScript();
